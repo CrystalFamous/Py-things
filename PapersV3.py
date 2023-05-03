896f1b2e994fb3ddf96cc17b6aa252bda48d2b93
CrystalFamous <113211306+CrystalFamous@users.noreply.github.com>
2023-05-03 22:24:37 +0300

    Update PapersV3.py

--- PapersV3.py
+++ PapersV3.py
@@ -1,15 +1,13 @@
 import webbrowser
-import keyboard
+Close = True
 Paper = ["https://papers.gceguide.com/A%20Levels/Accounting%20(9706)/"
         ,"https://papers.gceguide.com/A%20Levels/Business%20(9609)/"
         ,"https://papers.gceguide.com/A%20Levels/Computer%20Science%20(for%20final%20examination%20in%202021)%20(9608)/"
         ,"https://papers.gceguide.com/A%20Levels/Computer%20Science%20(for%20first%20examination%20in%202021)%20(9618)/"
         ,"https://papers.gceguide.com/A%20Levels/Economics%20(9708)/"]
-def openlink(Link:str):
-    webbrowser.open_new(str(Link))
-def openlink2(Link2:str,Link3:str):
-    webbrowser.open_new(str(Link2))
-    webbrowser.open_new(str(Link3))
+def openlinks(*links: str):
+    for link in links:
+        webbrowser.open_new(link)
 def routines():
     prompts = ["Enter year: ", "Which month? (s,w,m): ", "Which Paper: ", "qp or ms or both: "]
     inputs = []
@@ -26,11 +24,11 @@ def subject(subject_code: str, subject_index: int, year_range: str):
     year, mid, month, paper, choice = routines()
     if choice in ["qp", "1", "ms", "2"]:
         Link = Paper[subject_index] + year + "/" + subject_code + "_" + month + mid + "_" + choice + "_" + paper + ".pdf"
-        openlink(Link)
+        openlinks(Link)
     else:
         Link2 = Paper[subject_index] + year + "/" + subject_code + "_" + month + mid + "_qp_" + paper + ".pdf"
         Link3 = Paper[subject_index] + year + "/" + subject_code + "_" + month + mid + "_ms_" + paper + ".pdf"
-        openlink2(Link2, Link3)
+        openlinks(Link2, Link3)
     repeat()
 def acc():
     subject("9706", 0, "2015-2022")
@@ -56,7 +54,7 @@ def extract(sub:str):
     if sub[:4] in paper_dict:
         P, M, Y, MID = routinesv2(sub)
         Link = Paper[paper_dict[sub[:4]]] + Y + "/" + sub[:4] + "_" + M + MID + "_ms_" + P + ".pdf"
-        openlink(Link)
+        openlinks(Link)
         repeat()
 def main():
     sub = input("Acc,Bus,Eco,Cs\nOtherwise enter full code: ").lower()
@@ -72,9 +70,10 @@ def main():
     else:
         extract(sub)
 def repeat():
+    if Close:
+        exit()
     print("Do you want to search again?")
     reply = input()
     if reply == "yes" or reply == "y":
-        main()  
+        main() 
 main()
-
